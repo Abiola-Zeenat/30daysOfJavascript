825217860f83f51d0a6e7dcbb89a9ed5d67c0832
Abiola Zeenat Abdulsalam <81884414+Abiola-Zeenat@users.noreply.github.com>
2023-08-29 22:44:39 +0100

    Added day-7 level 2

--- Day-6/level-2.js
+++ Day-6/level-2.js
@@ -23,74 +23,158 @@ const webTechs = [
 
 const mernStack = ["MongoDB", "Express", "React", "Node"];
 
+
 //Question 1
 
+const randomId = function() {
+  return(Math.random().toString(36).substring(2));
+};
+console.log(randomId());
+
 //Question 2
+const randomHexaId = function(length=6) {
+  return('#'+Math.random().toString(16).substring(2, length+2));
+};
+console.log(randomHexaId());
 
 //Question 3
-function printArray(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-  }
-}
-printArray([8, 7, 6, 5, 4]);
+let randNum1 = Math.floor(Math.random()*255) + 1;
+let randNum2 = Math.floor(Math.random()*255) + 1;
+let randNum3 = Math.floor(Math.random()*255) + 1;
+console.log(`rgb(${randNum1},${randNum2},${randNum3})`);
 
 //Question 4
-function showDateTime() {
-  let date = new Date();
-  let dateTime = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
-  console.log(dateTime);
+const newArr = [];
+for (const country of countries) {
+  newArr.push(country.toUpperCase());
 }
-showDateTime();
+
+console.log(newArr);
 
 //Question 5
-function swapValues(x,y) {
-  console.log(`x = ${y}, y = ${x}`);
-    }
-    swapValues(3,4);
-    swapValues(4,5);
+const countryLength = [];
+
+for (const country of countries) {
+  countryLength.push(country.length);
+}
+
+console.log(countryLength);
 
 //Question 6
-function reverseArray(arr) {
-  let newArr =[]
-  for (let i = arr.length-1; i >= 0; i--) {
-     newArr.push(arr[i])
-    
-  }
-  return newArr;
+const arrInfo = [];
+for (const country of countries) {
+  let short = country.slice(0, 3);
+  short = short.toUpperCase();
+  let countryInfo = [country, short, country.length];
+  arrInfo.push(countryInfo);
 }
-console.log(reverseArray([1, 2, 3, 4, 5]))
-
-console.log(reverseArray(['A', 'B', 'C']))
-
+console.log(arrInfo);
 
 //Question 7
-function capitalizeArray(arr) {
-  let newArr =[]
-  for (let i = 0; i < arr.length; i++) {
-     newArr.push(arr[i].toUpperCase())
-    
+const landCountries = [];
+for (const country of countries) {
+  if (country.includes("land")) {
+    landCountries.push(country);
   }
-  return newArr;
 }
-console.log(capitalizeArray(['abiola', 'abdulsalam','lagos',]));
+if (landCountries.length == 0) {
+  console.log("All these countries are without land");
+} else {
+  console.log(landCountries);
+}
 
 //Question 8
-
+const iaCountries = [];
+for (const country of countries) {
+  if (country.endsWith("ia")) {
+    iaCountries.push(country);
+  }
+}
+if (iaCountries.length == 0) {
+  console.log("These countries ends without ia");
+} else {
+  console.log(iaCountries);
+}
 
 //Question 9
+const longestCountry = [];
+countryLength.sort();
+console.log(countryLength);
+for (const country of countries) {
+  if (country.length === countryLength[countryLength.length - 1]) {
+    longestCountry.push(country);
+  }
+}
+console.log(longestCountry);
 
 //Question 10
+const fiveLetterCountries = [];
+
+for (const country of countries) {
+  if (country.length === 5) {
+    fiveLetterCountries.push(country);
+  }
+}
+console.log(fiveLetterCountries);
 
 //Question 11
 
+const stackLength = [];
+for (const stack of webTechs) {
+  stackLength.push(stack.length);
+ }
+stackLength.sort((a, b)=> a - b);
+
+const longestStack = [];
+for (const stack of webTechs) {
+  if (stack.length === stackLength[stackLength.length - 1]) {
+    longestStack.push(stack);
+  }
+}
+
+console.log(longestStack);
+
 //Question 12
+const stackInfo = [];
+for (const stack of webTechs) {
+  
+  let techInfo = [stack, stack.length];
+  stackInfo.push(techInfo);
+}
+console.log(stackInfo);
 
 //Question 13
+const newarr = []
+for (const stack of mernStack) {
+  newarr.push(stack[0]);
+}
+console.log(newarr.join(''));
 
 //Question 14
-f;
+for (const stack of webTechs) {
+console.log(stack);
+}
 
 //Question 15
+const fruit = ['banana', 'orange', 'mango', 'lemon']
+const newFruit =[]
+for (let i = fruit.length-1; i >= 0; i--) {
+ newFruit.push(fruit[i]);
+  
+}
+console.log(newFruit);
 
 //Question 16
+const fullStack = [
+  ['HTML', 'CSS', 'JS', 'React'],
+  ['Node', 'Express', 'MongoDB']
+]
+for (const stack of fullStack) {
+ 
+  let newStack = stack.join();
+  
+console.log(newStack);
+  }
+  
+
+  export {countries, webTechs, mernStack};
--- Day-7/level-2.js
+++ Day-7/level-2.js
@@ -1,180 +1,136 @@
-const countries = [
-  "Albania",
-  "Bolivia",
-  "Canada",
-  "Denmark",
-  "Ethiopia",
-  "Finland",
-  "Germany",
-  "Hungary",
-  "Ireland",
-  "Japan",
-  "Kenya",
-];
-const webTechs = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "React",
-  "Redux",
-  "Node",
-  "MongoDB",
-];
+/*
 
-const mernStack = ["MongoDB", "Express", "React", "Node"];
 
+
+*/
 
 //Question 1
 
-const randomId = function() {
-  return(Math.random().toString(36).substring(2));
-};
-console.log(randomId());
-
 //Question 2
-const randomHexaId = function(length=6) {
-  return('#'+Math.random().toString(16).substring(2, length+2));
-};
-console.log(randomHexaId());
 
 //Question 3
-let randNum1 = Math.floor(Math.random()*255) + 1;
-let randNum2 = Math.floor(Math.random()*255) + 1;
-let randNum3 = Math.floor(Math.random()*255) + 1;
-console.log(`rgb(${randNum1},${randNum2},${randNum3})`);
+function printArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+printArray([8, 7, 6, 5, 4]);
 
 //Question 4
-const newArr = [];
-for (const country of countries) {
-  newArr.push(country.toUpperCase());
+function showDateTime() {
+  let date = new Date();
+  let dateTime = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+  console.log(dateTime);
 }
-
-console.log(newArr);
+showDateTime();
 
 //Question 5
-const countryLength = [];
-
-for (const country of countries) {
-  countryLength.push(country.length);
+function swapValues(x, y) {
+  console.log(`x = ${y}, y = ${x}`);
 }
-
-console.log(countryLength);
+swapValues(3, 4);
+swapValues(4, 5);
 
 //Question 6
-const arrInfo = [];
-for (const country of countries) {
-  let short = country.slice(0, 3);
-  short = short.toUpperCase();
-  let countryInfo = [country, short, country.length];
-  arrInfo.push(countryInfo);
+function reverseArray(arr) {
+  let newArr = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
 }
-console.log(arrInfo);
+console.log(reverseArray([1, 2, 3, 4, 5]));
+
+console.log(reverseArray(["A", "B", "C"]));
 
 //Question 7
-const landCountries = [];
-for (const country of countries) {
-  if (country.includes("land")) {
-    landCountries.push(country);
+function capitalizeArray(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i].toUpperCase());
   }
+  return newArr;
 }
-if (landCountries.length == 0) {
-  console.log("All these countries are without land");
-} else {
-  console.log(landCountries);
-}
+console.log(capitalizeArray(["abiola", "abdulsalam", "lagos"]));
 
 //Question 8
-const iaCountries = [];
-for (const country of countries) {
-  if (country.endsWith("ia")) {
-    iaCountries.push(country);
-  }
+function addItem(item) {
+  let itemArray = [];
+   itemArray.push(item);
+   return itemArray;
 }
-if (iaCountries.length == 0) {
-  console.log("These countries ends without ia");
-} else {
-  console.log(iaCountries);
-}
+
+console.log(addItem('rice'));
 
 //Question 9
-const longestCountry = [];
-countryLength.sort();
-console.log(countryLength);
-for (const country of countries) {
-  if (country.length === countryLength[countryLength.length - 1]) {
-    longestCountry.push(country);
-  }
+function removeItem(index){
+let arr = ['rice', 'banana', 'meat']
+arr.splice(index,1)
+return arr;
 }
-console.log(longestCountry);
+console.log(removeItem(1));
 
 //Question 10
-const fiveLetterCountries = [];
-
-for (const country of countries) {
-  if (country.length === 5) {
-    fiveLetterCountries.push(country);
-  }
+function sumOfNumbers(...nums){
+let sum = 0;
+for(const num of nums){
+  sum += num;  
 }
-console.log(fiveLetterCountries);
+return sum;
+}
+console.log(sumOfNumbers(5,3,8,7,11,28));
 
 //Question 11
-
-const stackLength = [];
-for (const stack of webTechs) {
-  stackLength.push(stack.length);
- }
-stackLength.sort((a, b)=> a - b);
-
-const longestStack = [];
-for (const stack of webTechs) {
-  if (stack.length === stackLength[stackLength.length - 1]) {
-    longestStack.push(stack);
+function sumOfOdds(...nums){
+  let sum = 0;
+  for(const num of nums){
+    if(num % 2 === 1){
+      sum += num
+    }
   }
+  return sum;
 }
-
-console.log(longestStack);
+console.log(sumOfOdds(5,3,8,7,11,28));
 
 //Question 12
-const stackInfo = [];
-for (const stack of webTechs) {
-  
-  let techInfo = [stack, stack.length];
-  stackInfo.push(techInfo);
+function sumOfEven(...nums){
+  let sum = 0;
+  for(const num of nums){
+    if(num % 2 === 0){
+      sum += num
+    }
+  }
+  return sum;
 }
-console.log(stackInfo);
+console.log(sumOfEven(5,3,8,7,11,28));
 
 //Question 13
-const newarr = []
-for (const stack of mernStack) {
-  newarr.push(stack[0]);
-}
-console.log(newarr.join(''));
+function evensAndOdds(num){
+  let odds = 0;
+  let evens = 0;
+  for (let i = 0; i <= num; i++) {
+    if (i % 2 === 0) {
+      evens += 1;
+    } else {
+      odds+=1;
+    }
+    
+  }
+  return (`The number of odds are ${odds}.\nThe number of evens are ${evens}`)
+};
+console.log(evensAndOdds(100));
 
 //Question 14
-for (const stack of webTechs) {
-console.log(stack);
-}
+function sum(...nums){
+  let sum = 0;
+  for(const num of nums){
+    sum += num;  
+  }
+  console.log(sum);
+  }
+sum(5,3,8,7,11,28);
+sum(1,2,3,4,5);
 
 //Question 15
-const fruit = ['banana', 'orange', 'mango', 'lemon']
-const newFruit =[]
-for (let i = fruit.length-1; i >= 0; i--) {
- newFruit.push(fruit[i]);
-  
-}
-console.log(newFruit);
 
 //Question 16
-const fullStack = [
-  ['HTML', 'CSS', 'JS', 'React'],
-  ['Node', 'Express', 'MongoDB']
-]
-for (const stack of fullStack) {
- 
-  let newStack = stack.join();
-  
-console.log(newStack);
-  }
-  
-
-  export {countries, webTechs, mernStack};
